5de9697a992af8ba3b69cf53214e56b53031928f
HafizWrG <132629477+HafizWrG@users.noreply.github.com>
2026-03-13 06:12:40 +0800

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ckcwwfgmvdvemwclphrl.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_SDQmjW8XE0hSn6NpV4g15A_RNIf4KQd';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
